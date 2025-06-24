7c0fb28dd6ef0b7ce466a12423cbe9225b76dd3f
TimWillame <Tim.Willame@outlook.com>
2025-06-24 17:16:31 +0200

    Delete Projet directory

--- Projet/ellipsometry/data_handling/__init__.py
+++
@@ -1,3 +0,0 @@
-# ellipsometry/data_handling/__init__.py
-
-from .data_handling import load_wavelengths, load_nk_data, save_supervector, save_supervector_lorentzian, save_supervector_maxwell_garnett, load_nk_data_maxwell_garnett
